f1605fc042c3afff7cd746ae5034c62513227ce5
isselcode <isselcode@gmail.com>
2026-03-11 09:57:45 -0600

    Implementación de ventana moderna con window_manager y snap_layouts

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:frontend_garzas/core/app/theme.dart';
 import 'package:frontend_garzas/src/auth/views/splash_view.dart';
+import 'package:snap_layouts/snap_layouts.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+import 'core/app/consts.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = WindowOptions(
+    size: Size(1366, 768),
+    minimumSize: Size(640, 480),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    windowButtonVisibility: false,
+    titleBarStyle: TitleBarStyle.hidden,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const MyApp());
 }
 
@@ -11,9 +33,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: lightTheme,
+      builder: (context, child) {
+        return Column(
+          children: [
+            SizedBox(
+              height: Size.fromHeight(kWindowCaptionHeight).height,
+              child: SnapLayoutsCaption(
+                icon: Image.asset(AppAssets.logo),
+                title: null,
+                backgroundColor: lightTheme.scaffoldBackgroundColor,
+                actions: [
+                  WindowCaptionAction(
+                    icon: Icon(Icons.favorite_outline),
+                    onPressed: () => print("Añadiendo a favorito"),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(child: child!)
+          ],
+        );
+      },
       home: SplashView(),
     );
   }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,15 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <screen_retriever_windows/screen_retriever_windows_plugin_c_api.h>
+#include <snap_layouts/snap_layouts_plugin.h>
+#include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  ScreenRetrieverWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ScreenRetrieverWindowsPluginCApi"));
+  SnapLayoutsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SnapLayoutsPlugin"));
+  WindowManagerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowManagerPlugin"));
 }
